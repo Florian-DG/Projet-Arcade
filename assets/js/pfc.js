87d702958df50acb418005107e64babb6a6770d7
Liza <liza.vandenbossche@gmail.com>
2025-11-05 10:39:25 +0100

    essaye début js

---
+++ assets/js/pfc.js
@@ -0,0 +1,15 @@
+const board = document.querySelector('#board');
+const resetBtn = document.querySelector('#reset');
+const message = document.querySelector('#message');
+let cells = Array(3).fill('');
+
+function render() {
+    board.innerHTML = '';
+    cells.forEach((cell, i) => {
+        const div = document.createElement('div');
+        div.className = 'cell';
+        div.textContent = cell;
+        div.addEventListener('click', () => play(i));
+        board.appendChild(div);
+    });
+}               
