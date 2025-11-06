eeee9c117c956b364df9b1127340003919c72dfb
Liza <liza.vandenbossche@gmail.com>
2025-11-06 15:58:40 +0100

    test du pfc

--- assets/js/pfc.js
+++ assets/js/pfc.js
@@ -3,13 +3,19 @@ const resetBtn = document.querySelector('#reset');
 const message = document.querySelector('#message');
 let cells = Array(3).fill('');
 
-function render() {
-    board.innerHTML = '';
-    cells.forEach((cell, i) => {
-        const div = document.createElement('div');
-        div.className = 'cell';
-        div.textContent = cell;
-        div.addEventListener('click', () => play(i));
-        board.appendChild(div);
-    });
+// function render() {
+//     board.innerHTML = '';
+//     cells.forEach((cell, i) => {
+//         const div = document.createElement('div');
+//         div.className = 'cell';
+//         div.textContent = cell;
+//         div.addEventListener('click', () => play(i));
+//         board.appendChild(div);
+//     });
+// }
+// render()
+
+function choose(chooseid){
+    console.log(`choisis : ${chooseid}`);
+    let pcchoose = Math.floor(Math.random()*2);
 }
